972903542664a9dcd47ca393c8d14b610f2474dc
yuanli <2865437316@qq.com>
2023-08-29 16:14:59 +0800

    fix: 项目首页ui修改及逻辑变更

---
+++ app/projects/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import siteData from "@/aurora.config";
+import Title from "@/components/title";
+
+const Projects = () => {
+    return (
+        <div className={'space-y-4'}>
+            <Title value={`项目`} />
+            {siteData.projects.map((item, index) => {
+                    return (
+                        <Link className={'block space-y-1.5'} key={index} href={item.url}>
+                            <div className={'font-medium underline underline-offset-4'}>
+                                {item.name}
+                            </div>
+                            <div className={'text-muted-foreground'}>{item.description}</div>
+                        </Link>
+                    )
+                })}
+        </div>
+    );
+}
+
+
+export default Projects

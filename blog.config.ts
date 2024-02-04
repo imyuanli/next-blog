fd406981a8b291d90e289a1473393c6301082930
yuanli <2865437316@qq.com>
2024-02-04 16:26:53 +0800

    feat: header实现

---
+++ blog.config.ts
@@ -0,0 +1,43 @@
+// Purpose: This file is used to configure the blog, including the author, title, description, and other settings.
+
+const siteData: any = {
+    author: "yuanli",  // author name
+    title: "NextBlog", // website title
+    description: "A minimalist blog created with Next.js ,Shadcn-ui and Tailwind.css", // website description
+    theme: "light", // light | dark
+    language: "zh-CN", // zh-CN | en
+    githubRepo: "https://github.com/imyuanli/next-blog", // your blog's github repo
+
+    // how to change the favicon of the website?
+    // change the app/favicon.ico file directly，or refer to the document below
+    // https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons
+
+    //header config
+    header: {
+        logo: "/logo.png",  //  /public/logo.png
+        title: "NextBlog", // header title
+
+        // navigation bar
+        routes: [
+            {
+                name: 'Blog',
+                value: '/blog'
+            },
+            {
+                name: 'Tags',
+                value: '/tags'
+            },
+            {
+                name: 'Projects',
+                value: '/projects'
+            },
+            {
+                name: 'About',
+                value: '/about'
+            }
+        ]
+    },
+
+}
+
+export default siteData
---
+++ components/header.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import siteData from "@/blog.config";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {Button} from "@/components/ui/button";
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import {useState} from "react";
+import {Separator} from "@/components/ui/separator";
+import {Github, Menu} from "lucide-react";
+
+const Header = () => {
+    const {githubRepo, header: {logo, title, routes}} = siteData
+
+    //高亮导航栏
+    const pathname = usePathname()
+    const active = routes.find((item: any) => item.value == '/' + pathname.split('/')[1])?.name
+
+    const [open, setOpen] = useState(false)
+
+    return (
+        <header className={'container flex justify-between py-4'}>
+            <div className={'flex justify-center items-center'}>
+                <Link className={'flex justify-center items-center mr-4'} href={'/'}>
+                    <Image
+                        src={logo}
+                        height={28}
+                        width={28}
+                        alt={'logo'}
+                    />
+                    <div className={'ml-1 text-lg font-semibold'}>
+                        {title}
+                    </div>
+                </Link>
+                <div className={'hidden md:block space-x-1'}>
+                    {routes.map((route: any) => (
+                        <Link href={route?.value}>
+                            <Button variant={active ? 'secondary' : 'ghost'} className={'text-base'}>
+                                {route.name}
+                            </Button>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <div className={'flex justify-center items-center space-x-1'}>
+                <div className={'md:hidden block'}>
+                    <Sheet open={open} onOpenChange={() => {
+                        setOpen(!open)
+                    }}>
+                        <SheetTrigger>
+                            <Button size={'icon'} variant={'ghost'}>
+                                <Menu size={20}/>
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side={'top'} className={'w-full space-y-4 p-12 text-sm'}>
+                            {routes.map((route: any, index: number) => (
+                                <div className={'space-y-4'}>
+                                    <Link href={route.value} onClick={() => {
+                                        setOpen(false)
+                                    }}>
+                                        <Button variant={active ? 'secondary' : 'ghost'} className={'text-base w-full'}>
+                                            {route.name}
+                                        </Button>
+                                    </Link>
+                                    {index != routes.length - 1 && <Separator/>}
+                                </div>
+                            ))}
+                        </SheetContent>
+                    </Sheet>
+                </div>
+                <Link href={githubRepo}>
+                    <Button size={'icon'} variant={'ghost'}>
+                        <Github size={20}/>
+                    </Button>
+                </Link>
+            </div>
+        </header>
+    );
+}
+
+export default Header;

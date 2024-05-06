00d4e870ef8e89bc21205c3c3e7ff93fa85c4ad6
yuanli <2865437316@qq.com>
2024-05-06 17:18:16 +0800

    fix: 修复不会马上出现title的bug

--- components/title.tsx
+++ components/title.tsx
@@ -3,27 +3,18 @@
 import {Separator} from "@/components/ui/separator";
 import {usePathname} from "next/navigation";
 import siteData from "@/blog.config";
-import {useEffect, useState} from "react";
 
 
 const Title = () => {
     const pathname = usePathname()
-    const [state, setState] = useState<any>(null)
-
-    useEffect(() => {
-        const nameArr = pathname.split('/')
-        const name = nameArr[nameArr.length - 1]
-        if (name) {
-            setState(siteData[name])
-        } else {
-            setState(siteData.home)
-        }
-    }, [pathname]);
+    const nameArr = pathname.split('/')
+    const name = nameArr[nameArr.length - 1]
+    const data = name ? siteData[name] : siteData.home
 
     return (
-        state && <div>
-          <h1>{state?.title}</h1>
-            {state?.description && <p className={'text-zinc-600'}>{state?.description}</p>}
+        data && <div>
+          <h1>{data?.title}</h1>
+            {data?.description && <p className={'text-zinc-600'}>{data?.description}</p>}
           <Separator/>
         </div>
     );

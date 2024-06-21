3e1a867db2e9b1a2da352ccb715bc6b3e127d1a6
yuanli <2865437316@qq.com>
2024-06-21 09:54:30 +0800

    fix: Missing Suspense boundary with useSearchParams

--- components/blog-content.tsx
+++ components/blog-content.tsx
@@ -7,7 +7,6 @@ import Time from "@/components/time";
 import Link from "next/link";
 import {ArrowRight} from "lucide-react";
 import {useSearchParams} from "next/navigation";
-import {Suspense} from "react";
 
 
 const BlogContent = ({posts}: any) => {
@@ -19,48 +18,46 @@ const BlogContent = ({posts}: any) => {
     }
 
     return (
-        <Suspense>
-            <div className={'pt-8 space-y-8'}>
-                {posts.map((post: any, index: number) => (
-                    <div className={'space-y-4'} key={index}>
-                        <div className={'space-y-4'}>
-                            <div className={'block md:hidden'}>
+        <div className={'pt-8 space-y-8'}>
+            {posts.map((post: any, index: number) => (
+                <div className={'space-y-4'} key={index}>
+                    <div className={'space-y-4'}>
+                        <div className={'block md:hidden'}>
+                            <Time date={post.time}/>
+                        </div>
+                        <CardTitle className={'not-prose'}>
+                            <Link className={'underline underline-offset-4'} href={`/blog/${post.id}`}>
+                                {post.title}
+                            </Link>
+                        </CardTitle>
+                        <div className={'flex flex-col md:flex-row md:space-x-4'}>
+                            <div className={'hidden md:block'}>
                                 <Time date={post.time}/>
                             </div>
-                            <CardTitle className={'not-prose'}>
-                                <Link className={'underline underline-offset-4'} href={`/blog/${post.id}`}>
-                                    {post.title}
-                                </Link>
-                            </CardTitle>
-                            <div className={'flex flex-col md:flex-row md:space-x-4'}>
-                                <div className={'hidden md:block'}>
-                                    <Time date={post.time}/>
-                                </div>
-                                <div className={'space-x-2'}>
-                                    {post?.tags?.map((tag: string, index: number) => (
-                                        <Link href={`/blog?tag=${tag}`}>
-                                            <Badge key={index} variant={currentTag == tag ? "default" : "secondary"}>
-                                                #{tag}
-                                            </Badge>
-                                        </Link>
-                                    ))}
-                                </div>
+                            <div className={'space-x-2'}>
+                                {post?.tags?.map((tag: string, index: number) => (
+                                    <Link href={`/blog?tag=${tag}`}>
+                                        <Badge key={index} variant={currentTag == tag ? "default" : "secondary"}>
+                                            #{tag}
+                                        </Badge>
+                                    </Link>
+                                ))}
                             </div>
                         </div>
-                        <CardDescription className={'text-base'}>
-                            {post.summary}
-                        </CardDescription>
-                        <div className={'flex justify-end'}>
-                            <Link href={`/blog/${post.id}`}>
-                                <Button>
-                                    Read More <ArrowRight size={16} className={'ml-2'}/>
-                                </Button>
-                            </Link>
-                        </div>
                     </div>
-                ))}
-            </div>
-        </Suspense>
+                    <CardDescription className={'text-base'}>
+                        {post.summary}
+                    </CardDescription>
+                    <div className={'flex justify-end'}>
+                        <Link href={`/blog/${post.id}`}>
+                            <Button>
+                                Read More <ArrowRight size={16} className={'ml-2'}/>
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 }
 
--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // todo This configuration option will be removed in a future major version.
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   // Optionally, add any other Next.js config below
 }
 

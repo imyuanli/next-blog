83c70ea4e027679ec4f0310fee9fa0fef1604577
yuanli <2865437316@qq.com>
2024-07-17 14:51:25 +0800

    feat: 自定义目录

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,16 +3,41 @@ import {MDXRemote} from "next-mdx-remote/rsc";
 import {Suspense} from "react";
 import Time from "@/components/time";
 import {getPostsData} from "@/app/server-utils";
-import DrawBack from "@/components/draw-back";
+import SideNav from "@/components/side-nav";
 import Comments from "@/plugins/comments";
 import shiki from 'rehype-shiki'
-import {toc} from "@jsdevtools/rehype-toc";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkGfm from "remark-gfm";
+import {remark} from "remark";
+import remarkFlexibleToc from "remark-flexible-toc";
+import readingTime from "reading-time";
+import remarkNormalizeHeadings from "remark-normalize-headings";
+import Toc from "@/components/toc";
+import {Button} from "@/components/ui/button";
+import {TableProperties} from "lucide-react";
+import {
+    Drawer,
+    DrawerContent,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from "@/components/ui/drawer";
 
-const getPost = (slug: string) => getPostsData().find((post: any) => post.id === slug)
+const getPost = async (slug: string) => {
+    const post: any = getPostsData().find((post) => post.id === slug)
+    // 获取目录数据
+    const file = await remark()
+        .use(remarkNormalizeHeadings)
+        .use(remarkFlexibleToc)
+        .process(post.content);
+    return {
+        ...post,
+        toc: file.data.toc,
+        stats: readingTime(post.content),
+    }
+}
 
 export async function generateStaticParams() {
     return getPostsData().map((post) => ({
@@ -21,50 +46,90 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata({params}: any) {
-    const post: any = getPost(params.slug)
+    const post: any = await getPost(params.slug)
     return {
         title: post.title,
         description: post.summary,
     }
 }
 
-export default function Post({params}: any) {
+export default async function Post({params}: any) {
     const {slug} = params
-    const post: any = getPost(slug)
+    const post: any = await getPost(slug)
     if (!post || post?.draft) notFound()
 
     return (
-        <div className={'relative'}>
-            <article>
-                <DrawBack/>
-                <div className={'mb-3 text-base text-zinc-400'}>
-                    <Time date={post.date}/> · {post.stats.words} words · {post.stats.text}
+        <div className={"w-full flex justify-center"}>
+            <div className={'w-full max-w-screen-lg  relative'}>
+                {/*mobile*/}
+                <div className={'block md:hidden sticky top-20 p-2 bg-white/80 backdrop-blur-md z-10 w-full shadow-sm'}>
+                    <div className={'flex justify-between'}>
+                        <div className={'space-x-4'}>
+                            <SideNav/>
+                        </div>
+                        <Drawer>
+                            <DrawerTrigger>
+                                <Button size={"icon"} variant={"ghost"}>
+                                    <TableProperties size={20}/>
+                                </Button>
+                            </DrawerTrigger>
+                            <DrawerContent>
+                                <DrawerHeader>
+                                    <DrawerTitle>
+                                        Table of contents
+                                    </DrawerTitle>
+                                </DrawerHeader>
+                                <Toc toc={post.toc}/>
+                            </DrawerContent>
+                        </Drawer>
+                    </div>
                 </div>
-                <Suspense fallback={<>Loading...</>}>
-                    <MDXRemote
-                        source={post.content}
-                        options={{
-                            mdxOptions: {
-                                remarkPlugins: [
-                                    remarkGfm,
-                                ],
-                                rehypePlugins: [
-                                    shiki,
-                                    rehypeSlug,
-                                    toc,
-                                    rehypeStringify,
-                                    rehypeAutolinkHeadings,
-                                ],
-                                remarkRehypeOptions: {
-                                    allowDangerousHtml: true
-                                }
-                            }
-                        }}
-                    />
-                </Suspense>
-            </article>
-            <Comments/>
+
+                {/*pc*/}
+                <div className={'px-4 pt-8'}>
+                    <div className={'sticky top-28 hidden xl:block'}>
+                        <div className={'absolute top-24 -left-16 -translate-x-full flex flex-col space-y-4'}>
+                            <SideNav/>
+                        </div>
+                        <div className={'absolute -right-8 translate-x-full'}>
+                            <div className={'w-full h-full max-h-[80vh] overflow-auto'}>
+                                <div className={'text-base font-bold mb-2'}>
+                                    Table of contents
+                                </div>
+                                <Toc toc={post.toc}/>
+                            </div>
+                        </div>
+                    </div>
+                    <article>
+                        <div className={'mb-3 text-base text-zinc-400'}>
+                            <Time date={post.date}/> · {post.stats.words} words · {post.stats.text}
+                        </div>
+                        <Suspense fallback={<>Loading...</>}>
+                            <MDXRemote
+                                source={post.content}
+                                options={{
+                                    mdxOptions: {
+                                        remarkPlugins: [
+                                            remarkNormalizeHeadings,
+                                            remarkGfm,
+                                        ],
+                                        rehypePlugins: [
+                                            shiki,
+                                            rehypeSlug,
+                                            rehypeStringify,
+                                            rehypeAutolinkHeadings,
+                                        ],
+                                        remarkRehypeOptions: {
+                                            allowDangerousHtml: true
+                                        }
+                                    }
+                                }}
+                            />
+                        </Suspense>
+                    </article>
+                    <Comments/>
+                </div>
+            </div>
         </div>
     )
 }
-
---
+++ components/toc.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import {Button} from "@/components/ui/button";
+import {cn} from "@/lib/utils";
+import {useState} from "react";
+
+const Toc = ({toc}: any) => {
+    const ml: any = {
+        2: '',
+        3: 'ml-4',
+        4: 'ml-8',
+        5: 'ml-12',
+        6: 'ml-16',
+    }
+    const [current, setCurrent] = useState<string>("")
+
+    const handleClick = (href: string) => {
+        setCurrent(href)
+        const id = href.replace('#', '')
+        const element: any = document.getElementById(id)
+        window.scrollTo({
+            top: element.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+
+    return (
+        <div className={'flex flex-col w-full space-y-1'}>
+            {
+                toc.map((item: any, index: number) => {
+                    return (
+                        <Button
+                            onClick={() => {
+                                handleClick(item.href)
+                            }}
+                            key={index}
+                            size={'sm'}
+                            variant={current == item?.href ? "secondary" : "ghost"}
+                            className={cn(ml[item.depth], 'text-sm justify-start')}
+                        >
+                            {item.value}
+                        </Button>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Toc;

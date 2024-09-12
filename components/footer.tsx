29797bb6d1e33beab868ee6a933b74c8c808c956
yuanli <2865437316@qq.com>
2024-09-12 17:24:41 +0800

    feat: 在footer和博客标题处新增 newsletter

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,14 +2,30 @@ import siteData from "@/blog.config";
 import SocialList from "@/components/social-list";
 import Link from "next/link";
 import {Separator} from "@/components/ui/separator";
+import Newsletter from "@/plugins/newsletter";
+import {Label} from "@/components/ui/label";
 
 const Footer = () => {
-    const {author, footer: {isShow, isShowPoweredBy}} = siteData
+    const {author, footer: {isShow, isShowPoweredBy}, newsletter: {title, description, position}} = siteData
 
     return (
         isShow && <div>
             <Separator/>
-            <footer className={'container py-4'}>
+            <footer className={'container py-8 space-y-4'}>
+                {position.footer &&
+                    <>
+                        <div className={"flex justify-between items-center flex-col md:flex-row space-y-4 md:space-y-0"}>
+                            <div className={"flex justify-center  items-center md:items-start flex-col"}>
+                                <Label className={'text-base'}>
+                                    {title && "Subscribe to the newsletter"}
+                                </Label>
+                                {description && <p className={"text-sm text-gray-500"}>{description}</p>}
+                            </div>
+                            <Newsletter/>
+                        </div>
+                        <Separator/>
+                    </>
+                }
                 <div
                     className={'w-full flex flex-col items-center space-y-4 md:flex-row md:justify-between md:space-y-0'}>
                     <div className={'md:hidden block'}>
--- components/title.tsx
+++ components/title.tsx
@@ -3,6 +3,7 @@
 import {Separator} from "@/components/ui/separator";
 import {usePathname, useSearchParams} from "next/navigation";
 import siteData from "@/blog.config";
+import Newsletter from "@/plugins/newsletter";
 
 
 const Title = () => {
@@ -10,20 +11,27 @@ const Title = () => {
     const nameArr = pathname.split('/')
     const name = nameArr[nameArr.length - 1]
 
+    const data = name ? siteData[name] : siteData.home
+
     // 如果是博客页面并带有tag参数
     const searchParams = useSearchParams()
     const tag = searchParams.get('tag')
-    if (name === 'blog' && tag) {
+    if (name === 'blog' || tag) {
         return <div className={'container pt-8'}>
-            <h1>{tag}</h1>
-            <p className={'text-zinc-600'}>
-                This is a list of all posts with the tag {tag}.
+            <h1>{tag ? tag : data?.title}</h1>
+            {data?.description &&
+                <p className={'text-zinc-600'}>
+                    {tag ? "This is a list of all posts with the tag {tag}." :
+                        data?.description}
+                </p>
+            }
+            <p>
+                <Newsletter/>
             </p>
             <Separator/>
         </div>
     }
 
-    const data = name ? siteData[name] : siteData.home
 
     return (
         data && <div className={'container pt-8'}>
